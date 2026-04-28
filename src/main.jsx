0dc4e9987319477f6c4899ba2bacec615d2562b3
leonardohsiehl <54863401+leonardohsiehl@users.noreply.github.com>
2026-04-29 02:06:59 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import MahjongSettlementMobileApp from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MahjongSettlementMobileApp />
+  </React.StrictMode>
+)
